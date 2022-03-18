d1d96d32a3c4496628b0fa56ce189169ae275e48
Isaac Babalola <info@isaacbabalola.co.uk>
2022-03-18 09:23:58 +0000

    adding govuk crest image

--- app/root.tsx
+++ app/root.tsx
@@ -124,15 +124,17 @@ function Header() {
 
 function Banner() {
   return (
-    <div className="govuk-phase-banner">
-      <p className="govuk-phase-banner__content">
-        <strong className="govuk-tag govuk-phase-banner__content__tag">
-          beta
-        </strong>
-        <span className="govuk-phase-banner__text">
-          This is a new service – your <a className="govuk-link" href="/">feedback</a> will help us to improve it.
-        </span>
-      </p>
+    <div className="govuk-width-container app-width-container">
+      <div className="govuk-phase-banner">
+        <p className="govuk-phase-banner__content">
+          <strong className="govuk-tag govuk-phase-banner__content__tag">
+            beta
+          </strong>
+          <span className="govuk-phase-banner__text">
+            This is a new service – your <a className="govuk-link" href="/">feedback</a> will help us to improve it.
+          </span>
+        </p>
+      </div>
     </div>
 );
 }
