389d74c0245bc8ff9f323322c545b8d507602dda
daniele-zurico <daniele.zurico@gmail.com>
2022-03-23 10:47:59 +0000

    rebase

--- app/components/hintTextInput.tsx
+++ app/components/hintTextInput.tsx
@@ -19,34 +19,32 @@ export const HintTextInput = ({
   onChange,
   error,
 }: HintTextInputProps) => (
-  <>
-    <div
-      className={`govuk-form-group ${
-        error === undefined ? "" : "govuk-form-group--error"
-      }`.trim()}
-    >
-      <label className="govuk-label" htmlFor={id}>
-        {label}
-      </label>
-      <div id={id_hint} className="govuk-hint govuk-!-width-two-thirds">
-        {hint}
-      </div>
-      {error !== undefined && (
-        <p id="event-name-error" className="govuk-error-message">
-          <span className="govuk-visually-hidden">Error:</span> {error.message}
-        </p>
-      )}
-      <input
-        className={`govuk-input govuk-!-width-two-thirds ${
-          error === undefined ? "" : "govuk-input--error"
-        }`.trim()}
-        id={id}
-        name={id}
-        type="text"
-        aria-describedby={id_hint}
-        value={value}
-        onChange={onChange}
-      />
+  <div
+    className={`govuk-form-group ${
+      error === undefined ? "" : "govuk-form-group--error"
+    }`.trim()}
+  >
+    <label className="govuk-label" htmlFor={id}>
+      {label}
+    </label>
+    <div id={id_hint} className="govuk-hint govuk-!-width-two-thirds">
+      {hint}
     </div>
-  </>
+    {error !== undefined && (
+      <p id="event-name-error" className="govuk-error-message">
+        <span className="govuk-visually-hidden">Error:</span> {error.message}
+      </p>
+    )}
+    <input
+      className={`govuk-input govuk-!-width-two-thirds ${
+        error === undefined ? "" : "govuk-input--error"
+      }`.trim()}
+      id={id}
+      name={id}
+      type="text"
+      aria-describedby={id_hint}
+      value={value}
+      onChange={onChange}
+    />
+  </div>
 );
