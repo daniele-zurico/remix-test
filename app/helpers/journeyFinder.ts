f929080af1d660b609f26ef7227a37c6fddd2128
AS <shinybrightpixels@gmail.com>
2022-06-06 15:59:09 +0100

    Update journeyFinder.ts

--- app/helpers/journeyFinder.ts
+++ app/helpers/journeyFinder.ts
@@ -1,15 +1,15 @@
-export const getJouneyName = (url: string): 'catch-certificate' | 'processing-statement' | 'storage-document' => {
-  if (url.includes('catch-certificate')) {
-    return 'catch-certificate';
+export const getJouneyName = (url: string): 'create-catch-certificate' | 'create-processing-statement' | 'create-storage-document' => {
+  if (url.includes('create-catch-certificate')) {
+    return 'create-catch-certificate';
   }
 
-  if (url.includes('processing-statement')) {
+  if (url.includes('create-processing-statement')) {
     return 'processing-statement';
   }
 
-  if (url.includes('storage-document')) {
+  if (url.includes('create-storage-document')) {
     return 'storage-document';
   }
 
-  return 'catch-certificate';
+  return null;
 }
